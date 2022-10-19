e40b40fdfb2ad803abc9d69ae0961248cd664464
Robert-Michalski <rmicha@softserveinc.com>
2022-10-19 14:15:20 +0200

    small style change to singlePost, added date to comments

--- app/components/SinglePost.js
+++ app/components/SinglePost.js
@@ -50,13 +50,13 @@ function Post() {
             </div>
           </div>
         </form>
-        <div className="comment mt-5 d-flex flex-row align-items-center ml-auto mr-auto">
+        <div className="comment mt-5 d-flex flex-row align-items-start ml-auto mr-auto">
           <div className="mr-3 col-2 text-center">
             <img src="https://www.nirix.com/uploads/files/Images/general/misc-marketing/avatar-2@2x.png" />
             <p className="font-weight-bold mt-2">Robur</p>
           </div>
-          <div className="comment-body">
-            <p>Hey ! Nice article. I really needed this. Have a nice day !</p>
+          <div className="comment-body mt-2 col-8">
+            <p>Hey ! Nice article. I really needed this. Have a nice day !Hey ! Nice article. I really needed this. Have a nice day ! Hey ! Nice article. I really needed this. Have a nice day !</p>
           </div>
           <div className="ml-auto d-flex flex-column align-self-start">
             <div className="align-items-start comment-date">Oct 19, 2022</div>
@@ -67,12 +67,12 @@ function Post() {
           </div>
         </div>
         <div className="mt-4"></div>
-        <div className="comment d-flex flex-row align-items-center ml-auto mr-auto">
+        <div className="comment d-flex flex-row align-items-start ml-auto mr-auto">
           <div className="mr-3 col-2 text-center">
             <img src="https://th.bing.com/th/id/OIP.PXqXsWvKQKc18fg18kL3GQHaHa?pid=ImgDet&rs=1" />
             <p className="font-weight-bold mt-2">Darek</p>
           </div>
-          <div className="comment-body">
+          <div className="comment-body mt-2 col-8">
             <p>Your article sucks. I will report you. Get banned</p>
           </div>
           <div className="ml-auto d-flex flex-column align-self-start">
