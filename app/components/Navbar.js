955da274056b657488802292bec8bdf5c1854853
Robert-Michalski <rmicha@softserveinc.com>
2022-10-19 09:18:03 +0200

    add pointer effect when hovering over functioning buttons

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -16,19 +16,19 @@ function Navbar() {
                 <li>CONSTRUCTORS</li>
               </Link>
               <ReactTooltip place="bottom" id="add" className="custom-tooltip" />{" "}
-              <Link to="/post/create" data-for="add" data-tip="Add Post" className="p-2 mr-4">
+              <Link to="/post/create" data-for="add" data-tip="Add Post" className="p-2 mr-4 ">
                 <span className="material-symbols-outlined"> add </span>
               </Link>
               <ReactTooltip place="bottom" id="search" className="custom-tooltip" />{" "}
-              <li data-for="search" data-tip="Search" className="p-2 mr-4" onClick={() => appDispatch({ type: "openSearch" })}>
+              <li data-for="search" data-tip="Search" className="p-2 mr-4 pointer" onClick={() => appDispatch({ type: "openSearch" })}>
                 <span className="material-symbols-outlined"> search </span>
               </li>
               <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{" "}
-              <li data-for="chat" data-tip="Chat" className="p-2 mr-4">
+              <li data-for="chat" data-tip="Chat" className="p-2 mr-4 pointer">
                 <span className="material-symbols-outlined"> chat </span>
               </li>
               <ReactTooltip place="bottom" id="settings" className="custom-tooltip" />{" "}
-              <li data-for="settings" data-tip="Settings" className="p-2 mr-4">
+              <li data-for="settings" data-tip="Settings" className="p-2 mr-4 pointer">
                 <span className="material-symbols-outlined"> settings </span>
               </li>
               <Link to="/login">
