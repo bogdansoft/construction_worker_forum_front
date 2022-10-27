1a4a0ab3fee4a2657d8a3772a7b1cd58745899a7
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-27 11:58:26 +0200

    Button submit

--- app/components/SingleChat.js
+++ app/components/SingleChat.js
@@ -10,16 +10,18 @@ function SingleChat() {
                     <div className="align-self-end sender mt-2">Hello</div>
                     <div className="align-self-end sender mt-2">What's up?</div>
                 </div>
-                <div className="container chat d-flex align-items-center mt-4">
-                    <form id="chatForm" className="container p-2 chat-input mr-4">
-                        <input type="text" size="95" className="no-outline chat-field" id="chatField"
-                               placeholder="Type a message..." autoComplete="off"/>
+                <div className="container chat align-items-center p-2 d-flex mt-4 mr-4">
+                    <form id="chatForm" className="container d-flex align-items-center">
+                        <div className="container chat-input">
+                            <input type="text" size="95" className="no-outline chat-field" id="chatField"
+                                   placeholder="Type a message..." autoComplete="off" autoFocus/>
+                        </div>
+                        <div className="mt-2 ml-auto">
+                            <button type="submit" className="material-symbols-outlined">
+                                send
+                            </button>
+                        </div>
                     </form>
-                    <div className="mt-2 ml-auto">
-                        <button className="material-symbols-outlined">
-                            send
-                        </button>
-                    </div>
                 </div>
             </div>
         </div>
