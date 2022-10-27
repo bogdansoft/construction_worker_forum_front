d4327b3562c5d8883d2420e8248ec51a2e6a5a76
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-27 10:49:27 +0200

    Base single chat component

---
+++ app/components/SingleChat.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function SingleChat() {
+    return (
+        <div className="main d-flex flex-column">
+            <div className="content d-flex mt-4 flex-column">
+                <div className="container flex-column d-flex">
+                    <div className="align-self-center font-weight-bold">data</div>
+                    <div className="align-self-start recipient mt-2">Hi!</div>
+                    <div className="align-self-end sender mt-2">Hello</div>
+                    <div className="align-self-end sender mt-2">What's up?</div>
+                </div>
+                <div className="container chat d-flex align-items-center mt-4">
+                    <form id="chatForm" className="container p-2 chat-input mr-4">
+                        <input type="text" size="95" className="no-outline chat-field" id="chatField"
+                               placeholder="Type a message..." autoComplete="off"/>
+                    </form>
+                    <div className="mt-2 ml-auto">
+                        <span className="material-symbols-outlined">
+                            send
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SingleChat;
