5f99b17c718e91cd267523fb01651ef23f8f66d7
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-31 17:15:31 +0100

    Functionality added I guess

--- app/components/TestChat.js
+++ app/components/TestChat.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {message} from "antd";
 import {useRecoilState, useRecoilValue} from "recoil";
 import {chatActiveContact, chatMessages, loggedInUser} from "../atom/GlobalState";
-import {findChatMessage} from "../util/ApiUtil";
+import {countNewMessages, findChatMessage, getUsers} from "../util/ApiUtil";
 
 let stompClient = null;
 const TestChat = (props) => {
@@ -53,9 +53,47 @@ const TestChat = (props) => {
                 setMessages(newMessages);
             });
         } else {
-            message.info("Received a new message from " + notification.senderName);
+            message.info("Received a new message from " + notification.senderName)
         }
         loadContacts();
+    };
+
+    const sendMessage = (msg) => {
+        if (msg.trim() !== "") {
+            const message = {
+                senderId: currentUser.id,
+                recipientId: activeContact.id,
+                senderName: currentUser.name,
+                recipientName: activeContact.name,
+                content: msg,
+                timestamp: new Date(),
+            };
+            stompClient.send("/app/chat", {}, JSON.stringify(message));
+
+            const newMessages = [...messages];
+            newMessages.push(message);
+            setMessages(newMessages);
+        }
+    };
+
+    const loadContacts = () => {
+        const promise = getUsers().then((users) =>
+            users.map((contact) =>
+                countNewMessages(contact.id, currentUser.id).then((count) => {
+                    contact.newMessages = count;
+                    return contact;
+                })
+            )
+        );
+
+        promise.then((promises) =>
+            Promise.all(promises).then((users) => {
+                setContacts(users);
+                if (activeContact === undefined && users.length > 0) {
+                    setActiveContact(users[0]);
+                }
+            })
+        );
     };
 
     return (
