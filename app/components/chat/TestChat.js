796e44a26742279fd301bf502d654581f2a9ddcf
Jakub Rogowski <jakerogowski33gmail.com>
2022-11-02 18:47:47 +0100

    New chat template

--- app/components/chat/TestChat.js
+++ app/components/chat/TestChat.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {message} from "antd";
-import {useRecoilState} from "recoil";
-import {chatActiveContact, chatMessages} from "../../atom/GlobalState";
-import {countNewMessages, findChatMessage, getUsers} from "../../util/ApiUtil";
+import React, {useContext, useEffect} from "react";
+import StateContext from "../../StateContext";
+import {useImmer} from "use-immer";
+
 
 let stompClient = null;
 
@@ -12,17 +11,19 @@ function TestChat() {
         username: localStorage.getItem("constructionForumUsername"),
         token: localStorage.getItem("constructionForumUserToken")
     }
-    const [text, setText] = useState("");
-    const [contacts, setContacts] = useState([]);
-    const [activeContact, setActiveContact] = useRecoilState(chatActiveContact);
-    const [messages, setMessages] = useRecoilState(chatMessages);
+    const appState = useContext(StateContext);
+    const [state, setState] = useImmer({
+        fieldValue: "",
+        chatMessages: []
+    });
 
     useEffect(() => {
-        if (localStorage.getItem("accessToken") === null) {
+        if (currentUser.token === null) {
             console.log("Not logged in!");
+        } else {
+            console.log("USING LOGGED IN USER TOKEN");
         }
         connect();
-        loadContacts();
     }, []);
 
     const connect = () => {
@@ -37,8 +38,7 @@ function TestChat() {
         console.log("connected");
         console.log(currentUser);
         stompClient.subscribe(
-            "/user/" + currentUser.id + "/queue/messages",
-            onMessageReceived
+            "/user/" + currentUser.id + "/queue/messages"
         );
     };
 
@@ -46,69 +46,56 @@ function TestChat() {
         console.log(err);
     };
 
-    const onMessageReceived = (msg) => {
-        const notification = JSON.parse(msg.body);
-        const active = JSON.parse(sessionStorage.getItem("recoil-persist")).chatActiveContact;
-
-        if (active.id === notification.senderId) {
-            findChatMessage(notification.id).then((message) => {
-                const newMessages = JSON.parse(sessionStorage.getItem("recoil-persist")).chatMessages;
-                newMessages.push(message);
-                setMessages(newMessages);
-            });
-        } else {
-            message.info("Received a new message from " + notification.senderName)
-        }
-        loadContacts();
-    };
-
     const sendMessage = (msg) => {
         if (msg.trim() !== "") {
             const message = {
                 senderId: currentUser.id,
-                recipientId: activeContact.id,
+                recipientId: 4,
                 senderName: currentUser.username,
-                recipientName: activeContact.username,
+                recipientName: "wolf123",
                 content: msg,
                 timestamp: new Date(),
             };
             stompClient.send("/app/chat", {}, JSON.stringify(message));
-
-            const newMessages = [...messages];
-            newMessages.push(message);
-            setMessages(newMessages);
         }
     };
 
-    const loadContacts = () => {
-        const promise = getUsers().then((users) =>
-            users.map((contact) =>
-                countNewMessages(contact.id, currentUser.id).then((count) => {
-                    contact.newMessages = count;
-                    return contact;
-                })
-            )
-        );
-
-        promise.then((promises) =>
-            Promise.all(promises).then((users) => {
-                setContacts(users);
-                if (activeContact === undefined && users.length > 0) {
-                    setActiveContact(users[0]);
-                }
-            })
-        );
-    };
-
     return (
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <p>hello!</p>
+        <div id="chat-wrapper"
+             className="container chat-wrapper chat-wrapper--is-visible shadow border-top border-left border-right">
+            <div className="chat-title-bar">
+                Chat with barksalot
+            </div>
+            <div id="chat" className="chat-log">
+                <div className="chat-self">
+                    <div className="chat-message">
+                        <div className="chat-message-inner">Hey, how are you?</div>
+                    </div>
+                    <img className="chat-avatar avatar-tiny"
+                         src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"/>
+                </div>
+
+                <div className="chat-other">
+                    <a href="#">
+                        <img className="avatar-tiny"
+                             src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"/>
+                    </a>
+                    <div className="chat-message">
+                        <div className="chat-message-inner">
+                            <a href="#">
+                                <strong>wolf123: </strong>
+                            </a>
+                            Hey, I am good, how about you?
+                        </div>
                     </div>
                 </div>
             </div>
+            <form id="chatForm" className="chat-form-inline">
+                <input type="text" placeholder="Type a message…" autoComplete="off" autoFocus/>
+                <button type="submit" className="material-symbols-outlined no-outline">
+                    send
+                </button>
+            </form>
         </div>
     );
 }
