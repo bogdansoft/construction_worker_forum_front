c4550c796d0531ee62a3d1f95b734ce66c8851fb
Jakub Rogowski <jakerogowski33gmail.com>
2022-11-08 15:30:21 +0100

    Added ChatContext on the whole app

--- app/Main.js
+++ app/Main.js
@@ -1,31 +1,31 @@
-import React, { useEffect } from "react"
-import { useImmerReducer } from "use-immer"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import jwtDecode from "jwt-decode"
-import ReactDOM from "react-dom/client"
-import DispatchContext from "./DispatchContext"
-import StateContext from "./StateContext"
-import { CSSTransition } from "react-transition-group"
-import Axios from "axios"
-import Navbar from "./components/Navbar"
-import Topics from "./components/Topics"
-import Footer from "./components/Footer"
-import Login from "./components/Login"
-import Search from "./components/Search"
-import RegisterForm from "./components/RegisterForm"
-import CreatePostForm from "./components/CreatePostForm"
-import CreateTopicForm from "./components/CreateTopicForm"
-import UserProfile from "./components/UserProfile"
-import ChangeBIO from "./components/ChangeBIO"
-import EditPost from "./components/EditPost"
-import EditTopic from "./components/EditTopic"
-import ViewSinglePost from "./components/ViewSinglePost"
-import FlashMessages from "./components/FlashMessages"
-import ViewSingleTopic from "./components/ViewSingleTopic"
-import NotFound from "./components/NotFound"
-import Logout from "./components/Logout"
+import React, { useEffect } from "react";
+import { useImmerReducer } from "use-immer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import ReactDOM from "react-dom/client";
+import DispatchContext from "./DispatchContext";
+import StateContext from "./StateContext";
+import { CSSTransition } from "react-transition-group";
+import Axios from "axios";
+import Navbar from "./components/Navbar";
+import Topics from "./components/Topics";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Search from "./components/Search";
+import RegisterForm from "./components/RegisterForm";
+import CreatePostForm from "./components/CreatePostForm";
+import CreateTopicForm from "./components/CreateTopicForm";
+import UserProfile from "./components/UserProfile";
+import ChangeBIO from "./components/ChangeBIO";
+import EditPost from "./components/EditPost";
+import EditTopic from "./components/EditTopic";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+import ViewSingleTopic from "./components/ViewSingleTopic";
+import NotFound from "./components/NotFound";
+import Logout from "./components/Logout";
 import Chat from "./components/chat/Chat";
-
+import { ChatProvider } from "./components/chat/Chat.context";
 
 Axios.defaults.baseURL = "http://localhost:8080";
 
@@ -40,35 +40,42 @@ function Main() {
       token: localStorage.getItem("constructionForumUserToken"),
       roles: [],
       isAdmin: false,
-      isSupport: false
+      isSupport: false,
     },
-    flashMessages: []
-  }
+    flashMessages: [],
+  };
 
   function ourReducer(state, action) {
     switch (action.type) {
       case "login":
-        state.loggedIn = true
-        state.user = action.data
-        state.user.roles = jwtDecode(state.user.token).roles
-        state.user.isAdmin = state.user.roles.includes("ADMINISTRATOR") ? true : false
-        state.user.isSupport = state.user.roles.includes("SUPPORT") ? true : false
-        break
+        state.loggedIn = true;
+        state.user = action.data;
+        state.user.roles = jwtDecode(state.user.token).roles;
+        state.user.isAdmin = state.user.roles.includes("ADMINISTRATOR")
+          ? true
+          : false;
+        state.user.isSupport = state.user.roles.includes("SUPPORT")
+          ? true
+          : false;
+        break;
       case "logout":
-        state.loggedIn = false
-        state.user.roles = []
-        state.user.isAdmin = false
-        state.user.isSupport = false
-        break
+        state.loggedIn = false;
+        state.user.roles = [];
+        state.user.isAdmin = false;
+        state.user.isSupport = false;
+        break;
       case "openSearch":
-        state.searchIsOpen = true
-        return
+        state.searchIsOpen = true;
+        return;
       case "closeSearch":
-        state.searchIsOpen = false
-        return
+        state.searchIsOpen = false;
+        return;
       case "flashMessage":
-        state.flashMessages.push({ value: action.value, messageType: action.messageType })
-        return
+        state.flashMessages.push({
+          value: action.value,
+          messageType: action.messageType,
+        });
+        return;
     }
   }
 
@@ -76,12 +83,20 @@ function Main() {
 
   useEffect(() => {
     if (state.loggedIn) {
-      localStorage.setItem("constructionForumUserId", state.user.id)
-      localStorage.setItem("constructionForumUsername", state.user.username)
-      localStorage.setItem("constructionForumUserToken", state.user.token)
-      initialState.user.roles = jwtDecode(localStorage.getItem("constructionForumUserToken")).roles
-      initialState.user.isAdmin = initialState.user.roles.includes("ADMINISTRATOR") ? true : false
-      initialState.user.isSupport = initialState.user.roles.includes("SUPPORT") ? true : false
+      localStorage.setItem("constructionForumUserId", state.user.id);
+      localStorage.setItem("constructionForumUsername", state.user.username);
+      localStorage.setItem("constructionForumUserToken", state.user.token);
+      initialState.user.roles = jwtDecode(
+        localStorage.getItem("constructionForumUserToken")
+      ).roles;
+      initialState.user.isAdmin = initialState.user.roles.includes(
+        "ADMINISTRATOR"
+      )
+        ? true
+        : false;
+      initialState.user.isSupport = initialState.user.roles.includes("SUPPORT")
+        ? true
+        : false;
     } else {
       localStorage.removeItem("constructionForumUserId");
       localStorage.removeItem("constructionForumUsername");
@@ -92,33 +107,43 @@ function Main() {
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
-        <BrowserRouter>
-          <FlashMessages messages={state.flashMessages} />
-          <Navbar />
-          <CSSTransition timeout={330} in={state.searchIsOpen} classNames="search-overlay" unmountOnExit>
-            <div className="search-overlay">
-              <Search />
-            </div>
-          </CSSTransition>
-          <Routes>
-            <Route path="/" element={<Topics />} />
-            <Route path="/profile/:username/*" element={<UserProfile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/user/create" element={<RegisterForm />} />
-            <Route path="/post/create" element={<CreatePostForm />} />
-            <Route path="/topic/create" element={<CreateTopicForm />} />
-            <Route path="/profile/changebio/:username" element={<ChangeBIO />} />
-            <Route path="/post/edit/:id" element={<EditPost />} />
-            <Route path="/topic/edit/:id" element={<EditTopic />} />
-            <Route path="/post/:id" element={<ViewSinglePost />} />
-            <Route path="/topic/:id" element={<ViewSingleTopic />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/chat" element={<Chat />} />
-          </Routes>
-          {state.loggedIn ? <Logout /> : null}
-          <Footer />
-        </BrowserRouter>
+        <ChatProvider>
+          <BrowserRouter>
+            <FlashMessages messages={state.flashMessages} />
+            <Navbar />
+            <CSSTransition
+              timeout={330}
+              in={state.searchIsOpen}
+              classNames="search-overlay"
+              unmountOnExit
+            >
+              <div className="search-overlay">
+                <Search />
+              </div>
+            </CSSTransition>
+            <Routes>
+              <Route path="/" element={<Topics />} />
+              <Route path="/profile/:username/*" element={<UserProfile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/user/create" element={<RegisterForm />} />
+              <Route path="/post/create" element={<CreatePostForm />} />
+              <Route path="/topic/create" element={<CreateTopicForm />} />
+              <Route
+                path="/profile/changebio/:username"
+                element={<ChangeBIO />}
+              />
+              <Route path="/post/edit/:id" element={<EditPost />} />
+              <Route path="/topic/edit/:id" element={<EditTopic />} />
+              <Route path="/post/:id" element={<ViewSinglePost />} />
+              <Route path="/topic/:id" element={<ViewSingleTopic />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/chat" element={<Chat />} />
+            </Routes>
+            {state.loggedIn ? <Logout /> : null}
+            <Footer />
+          </BrowserRouter>
+        </ChatProvider>
       </DispatchContext.Provider>
     </StateContext.Provider>
   );
--- app/components/chat/Chat.js
+++ app/components/chat/Chat.js
@@ -2,32 +2,29 @@ import React, { useMemo } from "react";
 import { localStorageService } from "../../services/localStorageService/localStorage.service";
 
 import { ContactList } from "./contactList/ContactList";
-import { ChatProvider } from "./Chat.context";
 import { ChatWindow } from "./ChatWindow";
 
 function Chat() {
   const currentUser = useMemo(() => localStorageService.getUser(), []);
 
   return (
-    <ChatProvider>
+    <div
+      id="chat-window"
+      className="container chat-window flex-nowrap d-flex align-items-center"
+    >
       <div
-        id="chat-window"
-        className="container chat-window flex-nowrap d-flex align-items-center"
+        id="chat-wrapper"
+        className="col-4 chat-wrapper chat-wrapper--is-visible shadow border-top border-left border-right"
       >
-        <div
-          id="chat-wrapper"
-          className="col-4 chat-wrapper chat-wrapper--is-visible shadow border-top border-left border-right"
-        >
-          <ContactList />
-        </div>
-        <div
-          id="chat-wrapper"
-          className="col-8 chat-wrapper chat-wrapper--is-visible shadow border-top border-left border-right"
-        >
-          <ChatWindow currentUser={currentUser} />
-        </div>
+        <ContactList />
       </div>
-    </ChatProvider>
+      <div
+        id="chat-wrapper"
+        className="col-8 chat-wrapper chat-wrapper--is-visible shadow border-top border-left border-right"
+      >
+        <ChatWindow currentUser={currentUser} />
+      </div>
+    </div>
   );
 }
 
