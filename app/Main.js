411633316606232160050ce45cd5d3b6c1daaa02
Robert-Michalski <rmicha@softserveinc.com>
2022-11-15 16:02:17 +0100

    configure ssl

--- app/Main.js
+++ app/Main.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { useImmerReducer } from "use-immer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import jwtDecode from "jwt-decode";
-import ReactDOM from "react-dom/client";
-import DispatchContext from "./DispatchContext";
-import StateContext from "./StateContext";
-import { CSSTransition } from "react-transition-group";
-import Axios from "axios";
-import Navbar from "./components/Navbar";
-import Topics from "./components/Topics";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Search from "./components/Search";
-import RegisterForm from "./components/RegisterForm";
-import CreatePostForm from "./components/CreatePostForm";
-import CreateTopicForm from "./components/CreateTopicForm";
-import UserProfile from "./components/UserProfile";
-import ChangeBIO from "./components/ChangeBIO";
-import EditPost from "./components/EditPost";
-import EditTopic from "./components/EditTopic";
-import ViewSinglePost from "./components/ViewSinglePost";
-import FlashMessages from "./components/FlashMessages";
-import ViewSingleTopic from "./components/ViewSingleTopic";
-import NotFound from "./components/NotFound";
-import Logout from "./components/Logout";
-import Chat from "./components/chat/Chat";
+import React, { useEffect } from "react"
+import { useImmerReducer } from "use-immer"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import jwtDecode from "jwt-decode"
+import ReactDOM from "react-dom/client"
+import DispatchContext from "./DispatchContext"
+import StateContext from "./StateContext"
+import { CSSTransition } from "react-transition-group"
+import Axios from "axios"
+import Navbar from "./components/Navbar"
+import Topics from "./components/Topics"
+import Footer from "./components/Footer"
+import Login from "./components/Login"
+import Search from "./components/Search"
+import RegisterForm from "./components/RegisterForm"
+import CreatePostForm from "./components/CreatePostForm"
+import CreateTopicForm from "./components/CreateTopicForm"
+import UserProfile from "./components/UserProfile"
+import ChangeBIO from "./components/ChangeBIO"
+import EditPost from "./components/EditPost"
+import EditTopic from "./components/EditTopic"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
+import ViewSingleTopic from "./components/ViewSingleTopic"
+import NotFound from "./components/NotFound"
+import Logout from "./components/Logout"
+import Chat from "./components/chat/Chat"
 
-Axios.defaults.baseURL = "http://localhost:8080";
+Axios.defaults.baseURL = "https://localhost:8443"
 
 function Main() {
   //
@@ -39,60 +39,57 @@ function Main() {
       token: localStorage.getItem("constructionForumUserToken"),
       roles: [],
       isAdmin: false,
-      isSupport: false,
+      isSupport: false
     },
-    flashMessages: [],
-  };
+    flashMessages: []
+  }
 
   function ourReducer(state, action) {
     switch (action.type) {
       case "login":
-        state.loggedIn = true;
-        state.user = action.data;
-        state.user.roles = jwtDecode(state.user.token).roles;
-        state.user.isAdmin = state.user.roles.includes("ADMINISTRATOR");
-        state.user.isSupport = state.user.roles.includes("SUPPORT");
-        break;
+        state.loggedIn = true
+        state.user = action.data
+        state.user.roles = jwtDecode(state.user.token).roles
+        state.user.isAdmin = state.user.roles.includes("ADMINISTRATOR")
+        state.user.isSupport = state.user.roles.includes("SUPPORT")
+        break
       case "logout":
-        state.loggedIn = false;
-        state.user.roles = [];
-        state.user.isAdmin = false;
-        state.user.isSupport = false;
-        break;
+        state.loggedIn = false
+        state.user.roles = []
+        state.user.isAdmin = false
+        state.user.isSupport = false
+        break
       case "openSearch":
-        state.searchIsOpen = true;
-        return;
+        state.searchIsOpen = true
+        return
       case "closeSearch":
-        state.searchIsOpen = false;
-        return;
+        state.searchIsOpen = false
+        return
       case "flashMessage":
         state.flashMessages.push({
           value: action.value,
-          messageType: action.messageType,
-        });
-        return;
+          messageType: action.messageType
+        })
+        return
     }
   }
 
-  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
   useEffect(() => {
     if (state.loggedIn) {
-      localStorage.setItem("constructionForumUserId", state.user.id);
-      localStorage.setItem("constructionForumUsername", state.user.username);
-      localStorage.setItem("constructionForumUserToken", state.user.token);
-      initialState.user.roles = jwtDecode(
-        localStorage.getItem("constructionForumUserToken")
-      ).roles;
-      initialState.user.isAdmin =
-        initialState.user.roles.includes("ADMINISTRATOR");
-      initialState.user.isSupport = initialState.user.roles.includes("SUPPORT");
+      localStorage.setItem("constructionForumUserId", state.user.id)
+      localStorage.setItem("constructionForumUsername", state.user.username)
+      localStorage.setItem("constructionForumUserToken", state.user.token)
+      initialState.user.roles = jwtDecode(localStorage.getItem("constructionForumUserToken")).roles
+      initialState.user.isAdmin = initialState.user.roles.includes("ADMINISTRATOR")
+      initialState.user.isSupport = initialState.user.roles.includes("SUPPORT")
     } else {
-      localStorage.removeItem("constructionForumUserId");
-      localStorage.removeItem("constructionForumUsername");
-      localStorage.removeItem("constructionForumUserToken");
+      localStorage.removeItem("constructionForumUserId")
+      localStorage.removeItem("constructionForumUsername")
+      localStorage.removeItem("constructionForumUserToken")
     }
-  }, [state.loggedIn]);
+  }, [state.loggedIn])
 
   return (
     <StateContext.Provider value={state}>
@@ -100,12 +97,7 @@ function Main() {
         <BrowserRouter>
           <FlashMessages messages={state.flashMessages} />
           <Navbar />
-          <CSSTransition
-            timeout={330}
-            in={state.searchIsOpen}
-            classNames="search-overlay"
-            unmountOnExit
-          >
+          <CSSTransition timeout={330} in={state.searchIsOpen} classNames="search-overlay" unmountOnExit>
             <div className="search-overlay">
               <Search />
             </div>
@@ -118,10 +110,7 @@ function Main() {
             <Route path="/user/create" element={<RegisterForm />} />
             <Route path="/post/create" element={<CreatePostForm />} />
             <Route path="/topic/create" element={<CreateTopicForm />} />
-            <Route
-              path="/profile/changebio/:username"
-              element={<ChangeBIO />}
-            />
+            <Route path="/profile/changebio/:username" element={<ChangeBIO />} />
             <Route path="/post/edit/:id" element={<EditPost />} />
             <Route path="/topic/edit/:id" element={<EditTopic />} />
             <Route path="/post/:id" element={<ViewSinglePost />} />
@@ -134,12 +123,12 @@ function Main() {
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
-  );
+  )
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<Main />);
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
