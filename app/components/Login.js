e4ad830682ba19b7a46c260d6e5d974e0dac9b07
Robert-Michalski <rmicha@softserveinc.com>
2022-11-15 10:54:45 +0100

    add tests, add userEvent package

--- app/components/Login.js
+++ app/components/Login.js
@@ -30,48 +30,46 @@ function Login() {
   }
 
   return (
-    <Router>
-      <div className="main d-flex flex-column container">
-        <div className="content d-flex mt-4">
-          <div className="d-flex flex-column col-6 align-items-center">
-            <div className="mt-4">
-              <h6 className="input-text text-center">
-                Log into Construction
-                <br />
-                Worker forum
-              </h6>
-            </div>
-            <form onSubmit={handleSubmit} className="d-flex flex-column container">
-              <div className="ml-5 form-label mt-4">
-                <span>Username</span>
-                <br />
-                <input onChange={e => setUsername(e.target.value)} type="text" className="form-button" />
-                <br />
-                <span>Password</span>
-                <br />
-                <input onChange={e => setPassword(e.target.value)} type="password" className="form-button" />
-              </div>
-              <div className="mt-4 align-self-end mr-5">
-                <button type="submit" className="nav-button">
-                  LOGIN
-                </button>
-              </div>
-            </form>
-            <div className="mt-3 form-label">
-              <span id="login-white">
-                No Account? Sign Up{" "}
-                <Link to="/user/create">
-                  <span id="login-blue">Here</span>
-                </Link>
-              </span>
-            </div>
+    <div className="main d-flex flex-column container">
+      <div className="content d-flex mt-4">
+        <div className="d-flex flex-column col-6 align-items-center">
+          <div className="mt-4">
+            <h6 className="input-text text-center">
+              Log into Construction
+              <br />
+              Worker forum
+            </h6>
           </div>
-          <div className="col-6 align-self-center">
-            <img src="https://static.timesofisrael.com/www/uploads/2017/12/managers.jpg" />
+          <form onSubmit={handleSubmit} className="d-flex flex-column container">
+            <div className="ml-5 form-label mt-4">
+              <span>Username</span>
+              <br />
+              <input onChange={e => setUsername(e.target.value)} type="text" className="form-button" />
+              <br />
+              <span>Password</span>
+              <br />
+              <input onChange={e => setPassword(e.target.value)} type="password" className="form-button" data-testid="password-field" />
+            </div>
+            <div className="mt-4 align-self-end mr-5">
+              <button type="submit" className="nav-button">
+                LOGIN
+              </button>
+            </div>
+          </form>
+          <div className="mt-3 form-label">
+            <span id="login-white">
+              No Account? Sign Up{" "}
+              <Link to="/user/create">
+                <span id="login-blue">Here</span>
+              </Link>
+            </span>
           </div>
         </div>
+        <div className="col-6 align-self-center">
+          <img src="https://static.timesofisrael.com/www/uploads/2017/12/managers.jpg" />
+        </div>
       </div>
-    </Router>
+    </div>
   )
 }
 
--- app/components/__tests__/login.test.js
+++ app/components/__tests__/login.test.js
@@ -1,14 +1,39 @@
 /**
  * @jest-environment jsdom
  */
-import { render, cleanup, screen, fireEvent, act } from "@testing-library/react"
+import { render, cleanup, screen, act, queryByAttribute } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import "@testing-library/jest-dom"
 import React from "react"
 import Login from "../Login"
+import { MemoryRouter, Router } from "react-router"
 
 afterEach(cleanup)
+
 test("test login page render", () => {
-  render(<Login />)
+  const dom = render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  )
   expect(screen.getByText("No Account? Sign Up")).toBeInTheDocument()
   expect(screen.getByRole("button", { type: "submit" })).toBeEnabled()
 })
+
+test("when provided good credentials, should render popup message and correct navbar", async () => {
+  const user = userEvent.setup()
+
+  const dom = render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  )
+
+  await user.type(screen.getByRole("textbox"), "jake123")
+  await user.type(screen.getByTestId("password-field"), "secret123")
+  expect(screen.getByRole("textbox")).toHaveValue("jake123")
+  expect(screen.getByTestId("password-field")).toHaveValue("secret123")
+  user.click(screen.getByRole("button"))
+})
+
+test("when provided bad credentials, should render popup message", () => {})
