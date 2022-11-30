a3e8959ee270cb73aef52b5dd98141dbae2096ea
Robert-Michalski <rmicha@softserveinc.com>
2022-11-30 11:47:21 +0100

    make user profile responsive

--- app/components/CreateTopicForm.js
+++ app/components/CreateTopicForm.js
@@ -6,7 +6,7 @@ import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
 import UnauthorizedAccessView from "./UnauthorizedAccessView"
 
-function CreateTopicForm(props) {
+function CreateTopicForm() {
   const [name, setName] = useState()
   const [description, setDescription] = useState()
   const navigate = useNavigate()
@@ -37,37 +37,42 @@ function CreateTopicForm(props) {
     }
   }
 
-  if (!appState.loggedIn || !appState.user.isAdmin) return <UnauthorizedAccessView />
+  // if (!appState.loggedIn || !appState.user.isAdmin) return <UnauthorizedAccessView />
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-label">
-        <div className="main d-flex flex-column container">
-          <Link className="text-primary medium font-weight-bold mb-3 mt-5" to={`/`}>
-            &laquo; Back to topics
-          </Link>
-          <div className="content d-flex flex-column mt-4">
-            <div className="d-flex flex-row">
-              <div className="ml-1 add-post-title">
-                <text style={{ fontSize: "40px" }}>Title:</text>
-                <input onChange={e => setName(e.target.value)} className="ml-3" type="text" style={{ fontSize: "20px" }} />
+      <div className="main d-flex flex-column container create-post">
+        <div className=" content d-flex flex-column mt-4">
+          <div className="d-flex flex-row">
+            <div>
+              <div className="ml-3 add-post-title">
+                Title: <input onChange={e => setName(e.target.value)} type="text" />
               </div>
-            </div>
-            <span className="form-group  ml-5 d-flex ">
-              <CSSTransition in={!name || name.length < 3} timeout={330} classNames="liveValidateMessage" unmountOnExit>
-                <div className="alert alert-danger mt-3 ml-3 liveValidateMessage">{!name ? "Empty title!" : "Title too short (min. 3 signs)"}</div>
-              </CSSTransition>
-            </span>
-            <div className="mt-3 ml-auto mr-auto">
-              <textarea onChange={e => setDescription(e.target.value)} className="post-textarea p-2 ml-5" rows="5" cols="99"></textarea>
-              <span className="form-group">
-                <CSSTransition in={!description} timeout={330} classNames="liveValidateMessage" unmountOnExit>
-                  <div className="alert alert-danger mt-3 liveValidateMessage">{"Empty description!"}</div>
+              <span className="form-group ml-5 d-flex" style={{ fontSize: "13px" }}>
+                <CSSTransition in={!name || name.length < 3 || name.length > 50} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+                  <div className="alert alert-danger mt-2 ml-5 liveValidateMessage">{!name || name.length > 50 ? "Empty title or too long (max. 50 sings)" : "Title too short (min. 3 signs)"}</div>
                 </CSSTransition>
               </span>
             </div>
-            <div className="ml-auto">
+          </div>
+          <div className="mt-5 mobile-toggle-inverse">
+            <textarea onChange={e => setDescription(e.target.value)} className="post-textarea p-2" rows="10" cols="40"></textarea>
+          </div>
+          <div className="mt-4 ml-auto mr-auto mobile-toggle">
+            <textarea onChange={e => setDescription(e.target.value)} className="post-textarea p-2" rows="10" cols="100"></textarea>
+          </div>
+          <span className="form-group ml-5 d-flex" style={{ fontSize: "13px" }}>
+            <CSSTransition in={!description || description.length > 1000} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+              <div className="alert alert-danger ml-5 liveValidateMessage">{"Empty description or too long (max. 1000 signs)"}</div>
+            </CSSTransition>
+          </span>
+          <div className="d-flex  flex-colum mt-5">
+            <div></div>
+            <div className="mobile-toggle ml-auto">
               <button className="nav-button">Create</button>
             </div>
+          </div>
+          <div className="mobile-toggle-inverse ml-auto">
+            <button className="nav-button">Create</button>
           </div>
         </div>
       </div>
