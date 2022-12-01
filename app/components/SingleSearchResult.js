73b366cd08bb53cab5d516e412865be24cea3775
Robert-Michalski <rmicha@softserveinc.com>
2022-12-01 10:03:51 +0100

    make search responsive

--- app/components/SingleSearchResult.js
+++ app/components/SingleSearchResult.js
@@ -10,8 +10,12 @@ function SingleSearchResult(props) {
     return (
       <div className="search-result mt-2 d-flex">
         <Link to={`/topic/${props.result.id}`}>{props.result.name}</Link>
-        <span className=" ml-auto mr-3">
-          Created: {date} By {props.result.user.username}{" "}
+        <span className=" ml-auto">
+          Created: {date}
+          <div className="mobile-toggle-inverse">
+            <div className="container"></div>
+          </div>
+          By {props.result.user.username}{" "}
         </span>
       </div>
     )
@@ -19,8 +23,12 @@ function SingleSearchResult(props) {
     return (
       <div className="search-result mt-2 d-flex">
         <Link to={`/post/${props.result.id}`}> {props.result.title}</Link>
-        <span className=" ml-auto mr-3">
-          Created: {date} By {props.result.user.username}{" "}
+        <span className=" ml-auto ">
+          Created: {date}
+          <div className="mobile-toggle-inverse">
+            <div className="container"></div>
+          </div>
+          By {props.result.user.username}{" "}
         </span>
       </div>
     )
