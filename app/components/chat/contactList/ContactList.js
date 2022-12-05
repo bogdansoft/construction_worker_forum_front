c8a8fdb6040a68d1855942c4a88e38801c8d06a6
Jakub Rogowski <jakerogowski33gmail.com>
2022-12-05 11:53:36 +0100

    Backend takes care of listing contacts

--- app/components/chat/contactList/ContactList.js
+++ app/components/chat/contactList/ContactList.js
@@ -1,21 +1,17 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext } from "react";
 import { ContactItem } from "./ContactItem";
 import ChatContext from "../Chat.context";
-import { localStorageService } from "../../../services/localStorageService/localStorage.service";
 
 export const ContactList = () => {
   const { contacts } = useContext(ChatContext);
-  const currentUserId = useMemo(() => localStorageService.getUser().id, []);
 
   return (
     <div>
       <div className="chat-title-bar mb-2">Messages</div>
       <div className="d-flex flex-column">
-        {contacts
-          .filter((c) => c.id.toString() !== currentUserId)
-          .map((contact) => (
-            <ContactItem contact={contact} />
-          ))}
+        {contacts.map((contact) => (
+          <ContactItem contact={contact} />
+        ))}
       </div>
     </div>
   );
