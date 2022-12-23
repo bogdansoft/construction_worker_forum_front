8f740261dd6047f072e64f90a09a057666c3809a
Jakub Rogowski <jakerogowski33gmail.com>
2022-12-23 16:18:22 +0100

    Added InfiniteScroll

--- app/components/notifications/NotificationBell.js
+++ app/components/notifications/NotificationBell.js
@@ -1,12 +1,37 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import NotificationContext from "./Notification.context";
-import { Badge, List, notification, Popover } from "antd";
+import { Badge, Divider, List, notification, Popover } from "antd";
 import { useNavigate } from "react-router-dom";
 import { BellOutlined } from "@ant-design/icons";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const NotificationBell = ({ currentUser }) => {
   const { notifications, setNotifications } = useContext(NotificationContext);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [pageCounter, setPageCounter] = useState(0);
   const navigate = useNavigate();
+
+  const loadMoreData = () => {
+    if (loading) return;
+
+    setLoading(true);
+    fetch(
+      `https://localhost:444/notification-service/notifications?userId=
+      ${currentUser.id}&page=${pageCounter + 1}`
+    )
+      .then((response) => response.json())
+      .then((body) => {
+        setData((prevState) => prevState.concat(body));
+        setLoading(false);
+        setPageCounter((prevState) => prevState + 1);
+      })
+      .catch(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    loadMoreData();
+  }, []);
 
   const handleServerEvent = (event) => {
     const jsonNotification = JSON.parse(event.data);
@@ -74,21 +99,39 @@ export const NotificationBell = ({ currentUser }) => {
   );
 
   const notificationList = (
-    <div>
-      <List
-        dataSource={notifications}
-        renderItem={(item) => (
-          <List.Item
-            className="item-not-read mt-2"
-            onClick={(e) => {
-              handleItemClick(item);
-              if (item.isRead) e.currentTarget.style.backgroundColor = "white";
-            }}
-          >
-            <b>{item.senderName}</b> {item.message}
-          </List.Item>
-        )}
-      />
+    <div
+      id="scrollableDiv"
+      style={{
+        height: 200,
+        overflow: "auto",
+        padding: "0 16px",
+        border: "1px solid rgba(140, 140, 140, 0.35)",
+      }}
+    >
+      <InfiniteScroll
+        next={loadMoreData}
+        hasMore={data.length <= notifications.length}
+        loader={<i>Loading...</i>}
+        dataLength={data.length}
+        endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+        scrollableTarget="scrollableDiv"
+      >
+        <List
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item
+              className="item-not-read mt-2"
+              onClick={(e) => {
+                handleItemClick(item);
+                if (item.isRead)
+                  e.currentTarget.style.backgroundColor = "white";
+              }}
+            >
+              <b>{item.senderName}</b> {item.message}
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
     </div>
   );
 
